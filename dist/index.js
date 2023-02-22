302118669aa1494a7ccb0c7123a3aa8a994112db
apicomplexa <->
2023-02-22 09:26:54 +0300

    Compiled files

---
+++ dist/index.js
@@ -0,0 +1,39 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const telegraf_1 = require("telegraf");
+const messages_1 = __importDefault(require("./messages"));
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const ICAL_SERVER_URL = process.env.ICAL_SERVER_URL;
+const bot = new telegraf_1.Telegraf(BOT_TOKEN);
+bot.start((ctx) => ctx.replyWithHTML(messages_1.default.info));
+bot.on('text', (ctx) => __awaiter(void 0, void 0, void 0, function* () {
+    const message = ctx.message.text.replace("-", "\-");
+    console.log(message);
+    const messageTextMatch = message.match(/http\:\/\/ruz\.nsmu\.ru\/\?group\=(.*)\/(.*)\&spec\=(.*)\#/);
+    if (messageTextMatch) {
+        console.log(`${messageTextMatch[1]}-${messageTextMatch[2]}-${messageTextMatch[3]}`);
+        const icalLink = `${ICAL_SERVER_URL}/${messageTextMatch[1]}/${messageTextMatch[2]}/${messageTextMatch[3]}`.replace("-", "\-");
+        const responseMessageText = (messages_1.default.sendIcalLink + '\n\n' + icalLink).replace("-", "\-");
+        yield ctx.replyWithHTML(responseMessageText);
+        yield ctx.replyWithHTML(messages_1.default.subscribtionInstruction.replace("-", "\-"));
+    }
+    else {
+        yield ctx.replyWithHTML(messages_1.default.invalidMessage);
+    }
+}));
+bot.launch();
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
---
+++ dist/messages.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    info: `Hello,
+
+This is a bot of the SSMU schedule synchronization project with electronic calendars
+In order to get started, send here a link to your schedule from the <a href="http://ruz.nsmu.ru">Site of NSMU</a>
+
+<a href="https://github.com/apicomplexa/nsmu-timetable">Project GitHub</a>`,
+    sendIcalLink: `Fine, It is link to subscribe to online calendars`,
+    subscribtionInstruction: `Past this link to your calendar 
+ 
+<a href="https://support.google.com/calendar/answer/37100?hl=en&co=GENIE.Platform%3DDesktop#:~:text=Use%20a%20link%20to%20add%20a%20public%20calendar">Google Calendar</a> <a href="https://support.google.com/calendar/answer/37100?hl=ru&co=GENIE.Platform%3DDesktop#:~:text=%D0%9A%D0%B0%D0%BA%20%D0%BF%D0%BE%D0%B4%D0%BF%D0%B8%D1%81%D0%B0%D1%82%D1%8C%D1%81%D1%8F%20%D0%BD%D0%B0%20%D0%BE%D0%B1%D1%89%D0%B5%D0%B4%D0%BE%D1%81%D1%82%D1%83%D0%BF%D0%BD%D1%8B%D0%B9%20%D0%BA%D0%B0%D0%BB%D0%B5%D0%BD%D0%B4%D0%B0%D1%80%D1%8C%20%D0%BF%D0%BE%20%D1%81%D1%81%D1%8B%D0%BB%D0%BA%D0%B5)]
+[">[RUS]</a>
+<a href="https://support.apple.com/guide/calendar/subscribe-to-calendars-icl1022/11.0/mac/13.0">Apple Calendar</a> <a href="https://support.apple.com/ru-ru/guide/calendar/icl1022/11.0/mac/13.0">[RUS]</a>
+Other calendars should have same functionality. Try to google it`,
+    invalidMessage: `Invalid message format
+In order to get started, send here a link to your schedule from the <a href="http://ruz.nsmu.ru">Site of NSMU</a>`
+};
